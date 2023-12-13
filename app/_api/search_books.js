abe7f7485b7c83b54893bf11eeee9d89f3746a7a
Oddisy <odunayoogunleye200@gmail.com>
2023-12-13 20:25:21 +0100

    fix: included cache control for the search

--- app/_api/search_books.js
+++ app/_api/search_books.js
@@ -1,35 +1,30 @@
 import {useState, useEffect} from "react";
 
-const useBookSearch = () => {
-	const apiUrl = "https://freetestapi.com/api/v1/books";
-	const [books, setBooks] = useState([]);
-	const [loading, setLoading] = useState(false);
-	const [error, setError] = useState(null);
+const searchBooks = (query) => {
+	setLoading(true);
+	setError(null);
 
-	const searchBooks = (query) => {
-		setLoading(true);
-		setError(null);
+	const searchUrl = `${apiUrl}?search=${query}`;
 
-		const searchUrl = `${apiUrl}?search=${query}`;
-
-		fetch(searchUrl)
-			.then((response) => {
-				if (!response.ok) {
-					throw new Error("Network response was not ok.");
-				}
-				return response.json();
-			})
-			.then((data) => {
-				setBooks(data);
-				setLoading(false);
-			})
-			.catch((error) => {
-				setError(error);
-				setLoading(false);
-			});
-	};
-
-	return {books, loading, error, searchBooks};
+	fetch(searchUrl, {
+		method: "GET",
+		headers: {
+			"Cache-Control": "no-store",
+			Pragma: "no-cache",
+		},
+	})
+		.then((response) => {
+			if (!response.ok) {
+				throw new Error("Network response was not ok.");
+			}
+			return response.json();
+		})
+		.then((data) => {
+			setBooks(data);
+			setLoading(false);
+		})
+		.catch((error) => {
+			setError(error);
+			setLoading(false);
+		});
 };
-
-export default useBookSearch;
